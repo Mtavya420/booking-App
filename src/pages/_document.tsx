4bc1ba42d8275d36b539c135af4f4cb282f68ef3
Dullah Mtavya <mtavya420@gmail.com>
2023-04-29 19:38:17 +0900

    update document file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,12 +2,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <link rel="stylesheet" href="..." />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
