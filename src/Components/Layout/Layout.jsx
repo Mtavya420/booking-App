916e9d508d114d24c7abfa72cc959a17e3a999ca
Dullah Mtavya <mtavya420@gmail.com>
2023-05-12 17:44:58 +0900

    Added Default SEO

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -20,10 +20,7 @@ export default function Layout({ children, props }) {
       <Head>
         <title>Elkpro Cut</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="Foreign barber shop in Seoul,South Korea"
-        />
+       
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,40 @@
 import "../styles/global.css";
 import "../styles/index.css";
+import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <>
+      <DefaultSeo
+        title="Elkpro Cut - Foreign Barber Shop in Korea"
+        description="Experience the best foreign barber services in Korea at Elkpro Cut. Our skilled barbers provide quality haircuts, shaves, and styling services to men of all ages and backgrounds. Visit us today and discover why we're the go-to destination for men's grooming in Seoul, South Korea."
+        openGraph={{
+          type: "website",
+          locale: "en_IE",
+          url: "https://elkpro.vercel.app/",
+          siteName: "Elkpro Cut",
+          images: [
+            {
+              url: "/img/ElkproLogo.png",
+              width: 800,
+              height: 600,
+              alt: "Best Barber Shop in Seoul",
+            },
+            {
+              url: "/img/ElkproLogo1.png",
+              width: 1200,
+              height: 630,
+              alt: "Hair Cut salon in Seoul",
+            },
+          ],
+
+          
+        }}
+        
+      />
       <Component {...pageProps} />
-    </ChakraProvider>
+    </>
   );
 }
